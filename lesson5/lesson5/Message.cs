2ac107d861358e08d6f0b1fb7793233cb3882762
Maksim Bogatov <maxbog@inbox.ru>
2019-07-10 21:06:02 +0300

    Работа со строками. Регулярные выражения.

---
+++ lesson5/lesson5/Message.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//2.	Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+//а) Вывести только те слова сообщения,  которые содержат не более n букв.
+//б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+//в) Найти самое длинное слово сообщения.
+//г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+
+
+
+namespace lesson5
+{
+    public static class Message
+    {
+        /// <summary>
+        /// Вывести только те слова сообщения,  которые содержат не более n букв.
+        /// </summary>
+        public static void PunktA(StringBuilder a)
+        {
+            //Console.WriteLine("Введите строку: ");
+            //StringBuilder a = new StringBuilder(Console.ReadLine());
+            //Console.WriteLine("Исходная строка: " + a);
+
+            Console.WriteLine("Введите количество символов в слове:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < a.Length;)
+                if (char.IsPunctuation(a[i])) a.Remove(i, 1);
+                else ++i;
+            string str = a.ToString();
+            string[] s = str.Split(' ');
+
+            Console.WriteLine("Искомые слова: ");
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (s[i].Length==n) Console.WriteLine(s[i]);
+            }
+
+        }
+
+        /// <summary>
+        /// Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+        /// </summary>
+
+        public static void PunktB(StringBuilder a)
+        {
+            //Console.WriteLine("Введите строку: ");
+            //StringBuilder a = new StringBuilder(Console.ReadLine());
+
+            Console.WriteLine("Введите последний символ в слове(слова с этим символом будут удалены):");
+            char n =Convert.ToChar(Console.ReadLine());
+            string empty = String.Empty;
+
+            for (int i = 0; i < a.Length;)
+                if (char.IsPunctuation(a[i])) a.Remove(i, 1);
+                else ++i;
+            string str = a.ToString();
+            string[] s = str.Split(' ');
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (s[i][s[i].Length - 1] == n) a.Replace(s[i], empty);
+            }
+
+            Console.WriteLine(a);
+
+        }
+
+        /// <summary>
+        /// Найти самое длинное слово сообщения.
+        /// </summary>
+
+        public static void PunktC(StringBuilder a)
+        {
+            //Console.WriteLine("Введите строку: ");
+            //StringBuilder a = new StringBuilder(Console.ReadLine());
+
+            string maxstr = String.Empty;
+
+            for (int i = 0; i < a.Length;)
+                if (char.IsPunctuation(a[i])) a.Remove(i, 1);
+                else ++i;
+            string str = a.ToString();
+            string[] s = str.Split(' ');
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (s[i].Length > maxstr.Length) maxstr=s[i];
+            }
+
+            Console.WriteLine($"Самое длинное слово: {maxstr}");
+
+        }
+
+        /// <summary>
+        /// Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+        /// </summary>
+
+        public static void PunktG(StringBuilder a)
+        {
+            //Console.WriteLine("Введите строку: ");
+            //StringBuilder a = new StringBuilder(Console.ReadLine());
+
+            string maxstr = String.Empty;
+
+            StringBuilder b = new StringBuilder();
+
+            for (int i = 0; i < a.Length;)
+                if (char.IsPunctuation(a[i])) a.Remove(i, 1);
+                else ++i;
+            string str = a.ToString();
+            string[] s = str.Split(' ');
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (s[i].Length >= maxstr.Length)
+                {
+                    maxstr = s[i];
+                    b = b.Append(maxstr);
+                }
+
+            }
+
+            Console.WriteLine($"Cтрока с помощью StringBuilder из самых длинных слов сообщения: {b}");
+
+        }
+    }
+}
---
+++ lesson5/lesson5/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+//Богатов Максим
+
+namespace lesson5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //1.Создать программу, которая будет проверять корректность ввода логина. Корректным логином будет строка от 2 до 10 символов, 
+            //содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+            //а) без использования регулярных выражений;
+
+            bool flag;
+            Console.WriteLine("Введите логин: ");
+            string login = Console.ReadLine();
+
+            try
+            {
+                // Проверка на длину
+                if (login.Length < 2 || login.Length > 10)
+                    throw new ArgumentException("Не корректная длина логина");
+
+                // Проверка первого символа на цифру
+                if (Char.IsDigit(login[0]))
+                    throw new ArgumentException("Не корректный первый символ логина");
+
+                for (int i = 0; i < login.Length; i++)
+                {
+                    if (!Char.IsLetterOrDigit(login[i]))
+                        throw new ArgumentException("Не корректный логин");
+
+                    if (((login[i] >= 'a') && (login[i] <= 'z')) || ((login[i] >= 'A') && (login[i] <= 'Z')) || ((login[i] >= '0') && (login[i] <= '9')))
+                        flag = true;
+                    else
+                        throw new ArgumentException("Не корректный логин"); ;
+
+                }
+
+                Console.WriteLine("Корректный логин");
+            }
+
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Не корректный логин");
+            }
+           //finally
+           // {
+           //     Console.ReadKey();
+           // }
+
+            //б) **с использованием регулярных выражений.
+
+            Console.WriteLine("Введите логин: ");
+            string login1 = Console.ReadLine();
+
+                Regex login_regex = new Regex(@"^[a-zA-Z][a-zA-Z0-9]{1,9}$");
+
+                if (login_regex.IsMatch(login1)) 
+                {
+                Console.WriteLine("Корректный логин");
+            }
+                else
+                {
+                Console.WriteLine("Не корректный логин");
+            }
+
+                Console.ReadKey();
+
+            //2.	Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+            //а) Вывести только те слова сообщения,  которые содержат не более n букв.
+            //б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+            //в) Найти самое длинное слово сообщения.
+            //г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+
+            try
+            {
+                Console.WriteLine("Введите строку: ");
+                StringBuilder a = new StringBuilder(Console.ReadLine());
+
+                Message.PunktA(a);
+                Message.PunktB(a);
+                Message.PunktC(a);
+                Message.PunktG(a);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
